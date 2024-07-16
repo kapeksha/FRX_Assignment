1223e1d5ef1f13d8ca3e973f7b6cd191d5787934
kapeksha <165630326+kapeksha@users.noreply.github.com>
2024-07-16 14:23:04 +0530

    Delete task_project directory

--- task_project/Task_Manager/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "task.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "taskauth"
